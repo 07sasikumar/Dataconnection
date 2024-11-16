e9beaa9f9492ea7d4cf979941e21c7ba9b1f09d6
07sasikumar <sasimech10@gmail.com>
2024-11-16 16:02:23 +0530

    Screenshot taken for login page

--- Databseconnectiont/src/test/java/screenshot/ScreenshotDeliveryforX.java
+++ Databseconnectiont/src/test/java/screenshot/ScreenshotDeliveryforX.java
@@ -17,33 +17,41 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class ScreenshotDeliveryforX {
 	public static void main(String[] ScreenShotDateandTime) throws IOException {
 
-		WebDriver driver = new FirefoxDriver();
-		driver.manage().window().maximize();
-		driver.get("https://deliveryforx.trymydemo.com/admin-login");// Lanch
-		WebElement login = driver.findElement(By.xpath("//input[@type=\"submit\"]"));
-		login.click();
-
+		System.out.println("process is started");
 		DateFormat dateformet = new SimpleDateFormat("dd.mm.yyyy_HH.ss");
 
 		// get current date
 		Date date = new Date();
-
 		String date1 = dateformet.format(date);
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.get("https://deliveryforx.trymydemo.com/admin-login");// Lanch
+		File login = new File("./DeliveryForX/LoginScreenshort" + date1 + ".png");
+		TakesScreenshot log = (TakesScreenshot) driver;
+		File firstSrc = log.getScreenshotAs(OutputType.FILE);
+		// using file tiles
+		FileUtils.copyFile(firstSrc, login);
+		System.out.println("Takes Screenshot Login page Sucessfully");
+		System.out.println("File Name :" + "LoginScreenshort" + date1);
+		WebElement login1 = driver.findElement(By.xpath("//input[@type=\"submit\"]"));
+		login1.click();
 
-		//System.out.println(date1);
+		// DateFormat dateformet = new SimpleDateFormat("dd.mm.yyyy_HH.ss");
+
+		// System.out.println(date1);
 		// set file location
-		File dec =new File ("./screenshort/Deliverforx(homepage)"+date1+".png");
-		
+		File dec = new File("./screenshort/Deliverforx(homepage)" + date1 + ".png");
+
 		TakesScreenshot ts = (TakesScreenshot) driver;
-		
-		File firstSrc = ts.getScreenshotAs(OutputType.FILE);
-		// using file tiles 
-				FileUtils.copyFile(firstSrc, dec);
-		// Program successfully completed 
+
+		File firstSrc1 = ts.getScreenshotAs(OutputType.FILE);
+		// using file tiles
+		FileUtils.copyFile(firstSrc1, dec);
+		// Program successfully completed
 		System.out.println("Takes Screenshot Sucessfully");
-		// print the file Name 
-		System.out.println("File Name :"+"Deliverforx(homepage)"+date1);
-		//Close the browser 
+		// print the file Name
+		System.out.println("File Name :" + "Deliverforx(homepage)" + date1);
+		// Close the browser
 		driver.close();
 
 	}
